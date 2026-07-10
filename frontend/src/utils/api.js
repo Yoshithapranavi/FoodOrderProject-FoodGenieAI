7ed05e62bb94af57a4858399ae5f6ffe1a3ff1b8
Yoshithapranavi <yoshithapranavi11@gmail.com>
2026-07-10 18:39:58 +0530

    Initial commit

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,13 @@
+// centralized API setup
+
+import axios from "axios";
+import qs from "qs";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000/api",
+  withCredentials: true,
+  paramsSerializer: (params) =>
+    qs.stringify(params, { arrayFormat: "repeat" }),
+});
+
+export default api;
